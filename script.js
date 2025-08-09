1f5b01d8855bc6558e24b260a14b1f5183e87e5f
wryamoh <wryamoh@gmail.com>
2025-08-09 09:00:44 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// This file will contain all the interactive logic for our app.
+// We will fill this in the next phase.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Application structure is ready. Logic will be added next.");
+});
